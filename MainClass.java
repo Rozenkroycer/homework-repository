108b0fb3f0cb4bbbb96e7a86209b080fc6283efb
Rozenkroycer <pavelbulic@gmail.com>
2022-05-02 11:17:32 +0200

    HW_1_Test_1

---
+++ MainClass.java
@@ -0,0 +1,11 @@
+public class MainClass {
+
+
+    public int getLocalNumber() {
+        //int number = 14;
+        return 14;
+    }
+
+}
+
+
---
+++ MainClassTest.java
@@ -0,0 +1,13 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MainClassTest extends MainClass
+{
+
+    @Test
+    public void testGetLocalNumber(){
+        int expected = 14;
+        int actual = this.getLocalNumber();
+        Assert.assertTrue("Number isn't 14",actual == expected);
+    }
+}
